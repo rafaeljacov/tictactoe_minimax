75ffa23d63e157c75bbaa427b81bb8d1a102d6dc
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-06-02 14:49:50 +0800

    finished animating line cross for win

--- main.lua
+++ main.lua
@@ -10,7 +10,8 @@ function love.load()
     crossPoints = {
         start = { x = 0, y = 0 },
         finish = { x = 0, y = 0 },
-        drawMode = nil
+        progress = 0,
+        speed = 1.7,
     }
 
     board = {
@@ -50,29 +51,26 @@ function love.mousepressed(x, y, button, _, _)
     end
 end
 
-function love.update()
+function love.update(dt)
     if cellsPlayed > 4 and not gameOver then
         local offset = 27
         local base = (cellSize / 2) - 4
         if board[1][1] ~= '' and board[1][1] == board[2][2] and board[2][2] == board[3][3] then
             winner = board[1][1]
-            crossPoints.drawMode = 'diagonal'
-            crossPoints.start.x = 0
-            crossPoints.start.y = 0
-            crossPoints.finish.x = boardSize
-            crossPoints.finish.y = boardSize
+            crossPoints.start.x = offset
+            crossPoints.start.y = offset
+            crossPoints.finish.x = boardSize - offset
+            crossPoints.finish.y = boardSize - offset
         elseif board[1][3] ~= '' and board[1][3] == board[2][2] and board[2][2] == board[3][1] then
             winner = board[1][3]
-            crossPoints.drawMode = 'diagonal'
-            crossPoints.start.x = boardSize
-            crossPoints.start.y = 0
-            crossPoints.finish.x = 0
-            crossPoints.finish.y = boardSize
+            crossPoints.start.x = boardSize - offset
+            crossPoints.start.y = offset
+            crossPoints.finish.x = offset
+            crossPoints.finish.y = boardSize - offset
         else
             for i = 1, #board do
                 if board[i][1] ~= '' and board[i][1] == board[i][2] and board[i][2] == board[i][3] then
                     winner = board[i][1]
-                    crossPoints.drawMode = 'horizontal'
                     crossPoints.start.x = offset
                     crossPoints.start.y = base + (i - 1) * cellSize
                     crossPoints.finish.x = boardSize - offset
@@ -80,7 +78,6 @@ function love.update()
                     break
                 elseif board[1][i] ~= '' and board[1][i] == board[2][i] and board[2][i] == board[3][i] then
                     winner = board[1][i]
-                    crossPoints.drawMode = 'vertical'
                     crossPoints.start.x = base + (i - 1) * cellSize
                     crossPoints.start.y = offset
                     crossPoints.finish.x = base + (i - 1) * cellSize
@@ -96,6 +93,11 @@ function love.update()
         elseif cellsPlayed == 9 then
             love.audio.play(tieSound)
             gameOver = true
+        end
+    elseif gameOver then
+        crossPoints.progress = crossPoints.progress + crossPoints.speed * dt
+        if crossPoints.progress > 1 then
+            crossPoints.progress = 1
         end
     end
 end
@@ -125,7 +127,7 @@ function love.draw()
     end
 
     if gameOver then
-        crossWin(crossPoints.start, crossPoints.finish, crossPoints.drawMode)
+        crossWin(crossPoints.start, crossPoints.finish)
     end
 end
 
@@ -148,18 +150,12 @@ function O(x, y)
     love.graphics.circle('line', x, y, 80)
 end
 
-function crossWin(start, finish, drawMode)
+function crossWin(start, finish)
     love.graphics.setLineWidth(20)
-    love.graphics.setColor(0, 0, 0, 0.9)
+    love.graphics.setColor(34 / 255, 43 / 255, 54 / 255)
 
-    love.graphics.line(start.x, start.y, finish.x, finish.y)
+    local currentX = start.x + (finish.x - start.x) * crossPoints.progress
+    local currentY = start.y + (finish.y - start.y) * crossPoints.progress
 
-    -- Experimental:
-    -- if drawMode == 'horizontal' then
-    --     for i = start.x, finish.x do
-    --         love.graphics.line(start.x, start.y, i, finish.y)
-    --     end
-    -- else
-    --     love.graphics.line(start.x, start.y, finish.x, finish.y)
-    -- end
+    love.graphics.line(start.x, start.y, currentX, currentY)
 end
