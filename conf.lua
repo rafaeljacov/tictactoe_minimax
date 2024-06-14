81f201ba1746506671424ad5521f1c768322efc4
Rafael Jacov Medel <medelrafjac@gmail.com>
2024-06-14 21:37:37 +0800

    move window size specification to conf.lua

---
+++ conf.lua
@@ -0,0 +1,8 @@
+function love.conf(t)
+    t.version = '11.5'
+    t.window.title = 'Tic Tac Toe'
+    t.window.width = 900 + 700
+    t.window.height = 900
+    t.modules.joystick = false
+    t.modules.physics = false
+end
